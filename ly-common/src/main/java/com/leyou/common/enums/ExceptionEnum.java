f650ec909b919aa17c35e4641b4621c094c22969
JW <>
2018-11-23 14:06:35 +0800

    添加通用工具

---
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -0,0 +1,25 @@
+package com.leyou.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum ExceptionEnum {
+
+//    PRICE_COUNT_BE_NULL(400,"价格不能为空！"),
+//    PRICE_COUNT_BE_NULL(400,"价格不能为空！"),
+//    PRICE_COUNT_BE_NULL(400,"价格不能为空！"),
+//    PRICE_COUNT_BE_NULL(400,"价格不能为空！"),
+    PRICE_COUNT_BE_NULL(400,"价格不能为空！")
+
+    ;
+    private int code;
+
+    private String msg;
+
+
+
+}
