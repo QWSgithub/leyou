2b3960d1123b6d1b0acfae009193e519dcc91b1b
JW <>
2018-11-30 18:17:59 +0800

    开始改造图片上传

---
+++ ly-upload/src/test/java/com/leyou/upload/FdfsTest.java
@@ -0,0 +1,28 @@
+package com.leyou.upload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FdfsTest {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private ThumbImageConfig thumbImageConfig;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        File file = new File();
+        
+    }
+
+}
